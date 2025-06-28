ad0dd3fc075a3f4ef03888f944f4d9db520e3133
lando <landonreekstin@gmail.com>
2025-06-27 19:15:30 -0500

    customize(blaney-pc): try out hyprland

--- hosts/blaney-pc/default.nix
+++ hosts/blaney-pc/default.nix
@@ -30,7 +30,7 @@
     };
     
     desktop = {
-      environment = "kde";
+      environment = "hyprland";
       displayManager = {
         enable = true; # false will go to TTY but not autolaunch a DE
         type = "ly"; # Or "greetd", "gdm", or "none" based on your preference for Optiplex
@@ -129,7 +129,6 @@
           shell = config.customConfig.user.shell;
         };
         desktop = config.customConfig.desktop.environment;
-        theme = config.customConfig.homeManager.theme.name;
         systemStateVersion = config.customConfig.system.stateVersion;
         packages = config.customConfig.packages.homeManager;
         services.gammastep = (config.customConfig.desktop.environment == "hyprland");
