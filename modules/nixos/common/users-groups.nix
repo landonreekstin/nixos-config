72e3c4427beb37a0ba02c1c13e36431b84def8d5
lando <landonreekstin@gmail.com>
2025-08-05 17:00:25 -0500

    tweak(ssh): updated ssh authorized_keys

--- modules/nixos/common/users-groups.nix
+++ modules/nixos/common/users-groups.nix
@@ -13,14 +13,12 @@ in
       # Ensure this user is the one these keys are for. If keys are specific to 'lando'
       # you might need: lib.mkIf (cfg.services.ssh.enable && cfg.user.name == "lando")
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILMzC5MBalzHuf4Bzd29KuvfaPSR91s7X+xg1OhZjnnu"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIkJdzUE5PWC2OPoGlKTMMRmf0ntDEgoppByYWb//deT"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJixKs6/A0swM2lkEuYacWtWNHRNio/X81y28S2CIkgj"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEDGIZ8pt4roFMBMGZCVOHcb3uibaREhOzSyGpL3AJ32"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILiUu/xNgEdcsIuaLekWDzty1JGyk2Asy7eqnbriPkmE"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHx/cuuV/hFddQhYcoTGLWfbxbNfMPBYzkmD5cnriFM"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYktBy6Y6JHbuCNaRAGI3xD8Zfdf9p+Ya+Z7W5EczjI"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFoL/AWCLKTRAPEvv/xVN3K5xwpWIDoqiG13jZlzQFVr"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSrv2XS/TnsS3msswQl7jvaGK6luOoca2HExxT1Hwip"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOIskZoPWjwNZiMshj1N4qsotaRneJH7Noa195EiZj3e"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2HwEWfiXzoAxXnMiP9FLZAbOgcdxhTtcWtxYxooNEQ"
 
     ];
     shell = cfg.user.shell;
