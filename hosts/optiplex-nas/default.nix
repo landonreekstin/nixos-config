81d07319f4108cfb615652ec8d9c08e0d1749ec3
lando <landonreekstin@gmail.com>
2025-08-08 20:10:52 -0500

    fix(bash color): color name typo

--- hosts/optiplex-nas/default.nix
+++ hosts/optiplex-nas/default.nix
@@ -46,7 +46,7 @@
       # This will be created during your installation.
       name = "lando"; # Or whatever user your script creates
       email = "landonreekstin@gmail.com";
-      shell.bash.color = "light-blue";
+      shell.bash.color = "bright-blue";
     };
     
     system = {
