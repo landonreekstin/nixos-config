0da2b83244e26f1497679d2ad82759721e5be534
lando <landonreekstin@gmail.com>
2025-09-10 18:06:47 -0500

    feat(atl-mini-pc): Stage new host for install

---
+++ hosts/atl-mini-pc/default.nix
@@ -0,0 +1,105 @@
+# ~/nixos-config/hosts/atl-mini-pc/default.nix
+{ inputs, pkgs, lib, config, ... }: # Standard module arguments. `config` is the final NixOS config.
+
+{
+  imports = [
+    # Hardware-specific configuration for this host
+    ./hardware-configuration.nix
+    
+    # Top level nixos modules import. All other nixos modules and option definitions are nested.
+    ../../modules/nixos/default.nix
+
+    # Host specific disk configuration
+    ./disko-config.nix
+
+  ];
+
+  # These values are for the options defined in `../../modules/nixos/common-options.nix`.
+  customConfig = {
+    
+    user = {
+      name = "heather";
+      email = "landonreekstin@gmail.com";
+      updateCmdPermission = false; 
+    };
+    
+    system = {
+      hostName = "atl-mini-pc"; # Actual hostname for this machine
+      stateVersion = "25.05"; # DO NOT CHANGE
+      timeZone = "America/New_York";
+      locale = "en_US.UTF-8"; 
+    };
+    
+    desktop = {
+      environments = [ "kde" ];
+      displayManager = {
+        enable = true; # false will go to TTY but not autolaunch a DE
+        type = "sddm";
+      };
+    };
+
+    hardware = {
+      nvidia = {
+        enable = false;
+      };
+    };
+
+    programs = {
+      partydeck.enable = false;
+      flatpak.enable = true;
+    };
+
+    homeManager = {
+      enable = true; # Enable Home Manager for this host
+    };
+
+    packages = {
+      nixos = with pkgs; [
+        vim
+        wget
+        fd
+        htop
+        pavucontrol
+      ];
+      homeManager = with pkgs; [
+        notes
+        chromium
+        firefox
+        libreoffice
+      ];
+    };
+
+    apps = {
+      defaultBrowser = "firefox";
+    };
+
+    profiles = {
+      gaming.enable = false;
+    };
+
+    services = {
+      ssh.enable = true;
+      vscodeServer.enable = true;
+    };
+
+  };
+
+  # Home Manager configuration for this Host
+  home-manager = lib.mkIf config.customConfig.homeManager.enable {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "hm-backup"; # Your existing setting
+    extraSpecialArgs = { inherit inputs; customConfig = config.customConfig; };
+    # Use the username from customConfig
+    users.${config.customConfig.user.name} = { pkgs', lib', config'', ... }: { # config'' here is the HM config being built for this user
+      imports = [
+        # === Common User Environment Modules ===
+        ../../modules/home-manager/default.nix
+
+        # === Theme Module ===
+        ../../modules/home-manager/themes/future-aviation/default.nix
+      ];
+    };
+  };
+  
+}
