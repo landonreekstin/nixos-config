e027223ce0f0c0b1f6a4e76dffdbf620088bd70e
lando <landonreekstin@gmail.com>
2025-09-23 08:39:07 -0500

    tweak(gaming-pc): remove redundant ckb-next option

--- hosts/gaming-pc/default.nix
+++ hosts/gaming-pc/default.nix
@@ -133,7 +133,6 @@
   };
 
   # === Additional nixos configuration for this host ===
-  #hardware.ckb-next.enable = true;
   #services.mullvad-vpn.enable = true;
   # Enable the Samba client-side name resolution daemon (nmbd).
   # This allows the PC to discover other Samba hosts (like optiplex-nas)
