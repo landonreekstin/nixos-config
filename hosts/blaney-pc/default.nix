932b46690ee15e705c00140914a037b5d947233a
lando <landonreekstin@gmail.com>
2025-09-24 17:54:10 -0500

    tweak(blaney-pc): remove mullvad vpn

--- hosts/blaney-pc/default.nix
+++ hosts/blaney-pc/default.nix
@@ -108,9 +108,6 @@
   };
   
   # === Additional nixos configuration for this host ===
-  services.mullvad-vpn.enable = true;
-  #services.g810-led.package = pkgs.g810-led; # Ensure the g810-led package is available
-  #services.g810-led.enable = true; # Enable Logitech G810 keyboard LED control
 
   # Home Manager configuration for this Host
   home-manager = lib.mkIf config.customConfig.homeManager.enable {
