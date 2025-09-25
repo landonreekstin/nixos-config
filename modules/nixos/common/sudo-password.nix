d1e78a901aafcdeb04855bba49e584a4fbee3482
lando <landonreekstin@gmail.com>
2025-09-24 22:17:56 -0500

    test(dual password): overwrite pam file to allow weak password

--- modules/nixos/common/sudo-password.nix
+++ modules/nixos/common/sudo-password.nix
@@ -10,7 +10,14 @@ in
   # Only apply these changes if the feature is enabled for the host.
   config = lib.mkIf cfg.user.sudoPassword {
 
-    security.pam.enablePasswordQualityChecks = false;
+    # The install script uses `chpasswd` to set the initial user password.
+    # We override its PAM configuration to be a minimal service that ONLY
+    # uses pam_unix to set the password. This bypasses the default password
+    # quality checks (pam_pwquality) and allows a weak password to be set
+    # for the graphical login, which is our specific goal for this host.
+    security.pam.services.chpasswd.text = ''
+      password required ${config.security.pam.package}/lib/security/pam_unix.so
+    '';
 
     security.pam.services.sudo = {
       text = ''
