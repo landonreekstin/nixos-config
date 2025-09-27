a6f350cda8ff52f79d5b02d8625787f6802c2cbe
lando <landonreekstin@gmail.com>
2025-09-27 18:22:07 -0500

    feat(blaney-pc): add thunar file manager

--- hosts/blaney-pc/default.nix
+++ hosts/blaney-pc/default.nix
@@ -115,6 +115,7 @@
   };
   
   # === Additional nixos configuration for this host ===
+  programs.thunar.enable = true;
 
   # Home Manager configuration for this Host
   home-manager = lib.mkIf config.customConfig.homeManager.enable {
