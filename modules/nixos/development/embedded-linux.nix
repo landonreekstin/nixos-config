925163de2685d1544b131a2e31cf2241b2676213
lando <landonreekstin@gmail.com>
2025-12-09 08:06:48 -0600

    feat(ulinux dev): Add toola for building u-boot

--- modules/nixos/development/embedded-linux.nix
+++ modules/nixos/development/embedded-linux.nix
@@ -5,7 +5,6 @@ let
   cfg = config.customConfig.profiles.development.embedded-linux;
 
   # Helper function to create a wrapper script for a tool.
-  # It takes a tool name (like "gcc") and a toolchain package set.
   make-tool-wrapper = tool-name: toolchain:
     pkgs.writeShellScriptBin "${toolchain.stdenv.cc.targetPrefix}${tool-name}" ''
       #!''${pkgs.bash}/bin/bash
@@ -28,10 +27,12 @@ in
         pkgsQEMU = pkgs.pkgsCross.raspberryPi;
         pkgsBBB = pkgs.pkgsCross.armv7l-hf-multiplatform;
 
-        # List of common tools we want to wrap for each toolchain
-        toolsToWrap = [ "gcc" "g++" "ld" "strip" "objcopy" ];
+        # EXPANDED list of tools to wrap. U-Boot needs all of these.
+        toolsToWrap = [ 
+          "gcc" "g++" "ld" "ar" "nm" "objcopy" "objdump" 
+          "ranlib" "readelf" "size" "strings" "strip" "as"
+        ];
 
-        # Create the wrapper packages for all tools for both toolchains
         qemuWrappers = map (tool-name: make-tool-wrapper tool-name pkgsQEMU) toolsToWrap;
         bbbWrappers = map (tool-name: make-tool-wrapper tool-name pkgsBBB) toolsToWrap;
 
@@ -39,25 +40,22 @@ in
       pkgs.mkShell {
         buildInputs = with pkgs; [
           # General build tools
-          autoconf automake bison bzip2 cmake flex gawk gcc gettext git gperf
-          help2man libtool ncurses patch python3 rsync texinfo unzip wget xz
+          autoconf automake bc bison bzip2 cmake dtc flex gawk gcc gettext git gperf
+          gnutls help2man libtool libuuid ncurses openssl patch python3 rsync swig texinfo unzip wget xz
           qemu_full ubootTools
 
-          # The actual GDB/Binutils from the toolchains (these don't conflict)
+          # The actual GDB/Binutils from the toolchains
           pkgsQEMU.gdb pkgsQEMU.binutils
           pkgsBBB.gdb pkgsBBB.binutils
-        ] ++ qemuWrappers ++ bbbWrappers; # Add our new wrappers to the inputs
+        ] ++ qemuWrappers ++ bbbWrappers;
 
         shellHook = ''
           echo "--------------------------------------------------------"
           echo "Entered Unified Embedded Linux Dev Shell."
           echo
           echo "Explicit compilers are now in your PATH."
-          echo "Use them directly, for example:"
-          echo "  ${pkgsBBB.stdenv.cc.targetPrefix}gcc -o hello hello.c"
-          echo "  ${pkgsQEMU.stdenv.cc.targetPrefix}gcc -o hello hello.c"
-          echo
-          echo "Tab completion for '${pkgsBBB.stdenv.cc.targetPrefix}' will show all tools."
+          echo "To build U-Boot for BBB, use:"
+          echo "  export CROSS_COMPILE=${pkgsBBB.stdenv.cc.targetPrefix}"
           echo "--------------------------------------------------------"
         '';
       };
